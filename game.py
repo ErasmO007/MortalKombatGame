a7bc9d4dc5ed12e26ea65a160234329fe8e96ac8
Jacobesc2004 <utm22030618@utma.edu.mx>
2023-06-22 19:43:57 -0600

    feat: The class 'MKCharacter' is created with three functions in it

--- game.py
+++ game.py
@@ -35,3 +35,39 @@ class MKCharacterType:
         - bool: True if this type is weak against the other type, False otherwise.
         """
         return other.name in self.strengths
+    
+    class MKCharacter:
+        def __init__(self, name, types):
+            """
+            Represents a Mortal Kombat character with a name and types.
+
+             Parameters:
+             - name (str): The name of the character.
+             - types (list): A list of MKCharacterType objects representing the types of the character.
+            """
+            self.name = name
+            self.types = types
+            self.max_health = len(types) * 50
+            self.current_health = self.max_health
+            self.is_fainted = False
+
+        def __repr__(self):
+            """
+            Returns a string representation of the character.
+
+            Returns:
+            - str: The string representation of the character.
+            """
+            type_names = "/".join([t.name for t in self.types])
+            return f"{self.name} ({type_names})"
+
+        def lose_health(self, damage):
+            """
+              Decreases the character's current health by the specified damage.
+
+              Parameters:
+              - damage (int): The amount of damage to subtract from the current health.
+            """
+            self.current_health = max(0, self.current_health - damage)
+            if self.current_health == 0:
+                self.faint()
